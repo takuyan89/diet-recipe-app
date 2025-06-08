0917359117883d1c9a4f3a5ff699a1863d4a03f3
Takuya Nomura <git.takuya89@gmail.com>
2025-06-08 19:20:05 +0200

    レシピカード修正

--- src/app/components/ui/card/RecipeCard.tsx
+++ src/app/components/ui/card/RecipeCard.tsx
@@ -4,8 +4,17 @@ import Link from 'next/link';
 export const RecipeCard = ({ recipe }: { recipe: Recipe }) => {
   return (
     <Link href={`/recipe/${recipe.id}`} key={recipe.id} className='no-underline'>
-      <div className='p-4 border rounded shadow w-72 h-52 flex items-end'>
-        <h2 className='text-xl font-bold'>{recipe.title}</h2>
+      <div className='p-4 border rounded shadow w-72 h-52 flex flex-col justify-end bg-white overflow-hidden relative'>
+        {recipe.imageUrl && (
+          <img
+            src={recipe.imageUrl}
+            alt={recipe.title}
+            className='absolute top-0 left-0 w-full h-full object-cover opacity-60'
+          />
+        )}
+        <div className='relative z-10  backdrop-blur-sm p-2 rounded'>
+          <h2 className='text-xl font-bold text-black'>{recipe.title}</h2>
+        </div>
       </div>
     </Link>
   );
