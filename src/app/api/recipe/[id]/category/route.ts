f76cc018854bd0fce32b29b620890e03fea0d22a
Takuya Nomura <git.takuya89@gmail.com>
2025-06-09 11:14:39 +0200

    apiの修正

--- src/app/api/recipe/[id]/category/route.ts
+++ src/app/api/recipe/[id]/category/route.ts
@@ -11,7 +11,6 @@ export async function POST(
     };
   }
 ) {
-  const resolvedParams = await params;
-
-  return await createRecipeCategory(request, resolvedParams.id);
+  const { id } = params;
+  return await createRecipeCategory(request, id);
 }
